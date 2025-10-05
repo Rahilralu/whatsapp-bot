3e53959eeb68bba6efdf743d2940fc9aadd2dc9d
Rahilralu <nazarrahil0000@gmail.com>
2025-10-05 06:44:33 +0530

    Changed geminin model

--- services/gemini.js
+++ services/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const client = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = client.getGenerativeModel({ model: "gemini-2.5-flash" });
+const model = client.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function generateReply(userMessage) {
   try {
